06f7d54f0724b674f916707bc83ec6c73837a7be
sinanrah <129244128+sinanrah@users.noreply.github.com>
2024-03-31 01:18:08 +0000

    init free save

--- include/hw3.h
+++ include/hw3.h
@@ -3,8 +3,13 @@
 typedef struct GameState
 {
     // define your struct here
+    int rows;
+    int cols;
+    char **board;         
+    int **heights;
 } GameState;
 
+void print_game_state(GameState* gs, int rows); // COMMENT OUT WHEN DONE
 GameState* initialize_game_state(const char *filename);
 GameState* place_tiles(GameState *game, int row, int col, char direction, const char *tiles, int *num_tiles_placed);
 GameState* undo_place_tiles(GameState *game);
